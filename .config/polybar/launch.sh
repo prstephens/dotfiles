48db8e3fe0449a4cb637b69263b78dd01393df27
pr.stephens@gmail.com <pr.stephens@gmail.com>
2020-05-20 12:12:30 +0100

    added default conky config and general tidy up

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,7 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log
 polybar top >>/tmp/polybar1.log 2>&1 &
-#polybar bottom >>/tmp/polybar2.log 2>&1 &
 
 echo "Bars launched..."
