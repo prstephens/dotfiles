8905545785a6f45f9c0abf0025c2313316f6945c
Paul <paul@archpc.localdomain>
2020-06-29 14:51:53 +0100

    lush

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,4 +9,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+#polybar main -c ~/.config/polybar/config.ini &
+polybar power -c ~/.config/polybar/config.ini &\
+polybar datetime -c ~/.config/polybar/config.ini &\
+polybar launch -c ~/.config/polybar/config.ini &\
+polybar workspaces -c ~/.config/polybar/config.ini &\
+polybar spacer -c ~/.config/polybar/config.ini &\
+
