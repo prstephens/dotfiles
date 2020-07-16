6e2126e0c1538ae516c57eaf27af617967a8b178
Paul <paul@archpc.localdomain>
2020-07-16 21:12:07 +0100

    proper lush

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,6 +9,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
+
+polybar datetime-bg &\
 polybar power &\
 polybar datetime &\
 polybar launch &\
